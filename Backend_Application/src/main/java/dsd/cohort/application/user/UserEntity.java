fcf450aff6d67c0079a0396673ef5113b93399cb
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2024-04-09 22:28:27 -0400

    Remove unused import and dependency

--- Backend_Application/src/main/java/dsd/cohort/application/user/UserEntity.java
+++ Backend_Application/src/main/java/dsd/cohort/application/user/UserEntity.java
@@ -2,7 +2,6 @@ package dsd.cohort.application.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
