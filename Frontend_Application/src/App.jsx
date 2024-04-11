445aff200841e165940a6ed6526c0ac7d619fc47
Belle Duran <bduran04@gmail.com>
2024-04-11 09:29:51 -0500

    created dashboard page WIP

--- Frontend_Application/src/App.jsx
+++ Frontend_Application/src/App.jsx
@@ -6,6 +6,7 @@ import { Outlet, Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Login from "./routes/login/login.component";
 import Register from "./routes/register/register.component";
+import Dashboard from "./routes/dasboard/dashboard.component";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
---
+++ Frontend_Application/src/routes/dasboard/dashboard.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/navbar.component.jsx";
+import Grid from '@mui/material/Grid';
+import Typography from "@mui/material/Typography";
+import Menu from "../../components/menu.component.jsx";
+
+function DashboardPage() {
+    // Replace 'userName' with the actual user's name from state or props
+    const userName = 'Mittens';
+
+    return (
+        <>
+            <Outlet />
+            <Navbar />
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item>
+                    <Typography variant="h3" gutterBottom>Welcome, {userName}'s Dashboard</Typography>
+                </Grid>
+                <Grid item>
+                    <Menu />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default DashboardPage;
