89de71b5b18adc4d353e2484c799d6632ee6b877
em-ess <michssong@gmail.com>
2024-04-13 15:04:28 -0500

    feature/addAppName

--- Frontend_Application/src/App.jsx
+++ Frontend_Application/src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Outlet, Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Login from "./routes/login/login.component";
 import Register from "./routes/register/register.component";
 import Dashboard from "./routes/dasboard/dashboard.component";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
