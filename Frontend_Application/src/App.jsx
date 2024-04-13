adefd1654582b05a774d5a5d0c410a23b0ef6b94
em-ess <michssong@gmail.com>
2024-04-13 16:07:53 -0500

    fix pathing

--- Frontend_Application/src/App.jsx
+++ Frontend_Application/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/myrecipes" element={<MyRecipes />} />
       </Routes>
     </>
   );
