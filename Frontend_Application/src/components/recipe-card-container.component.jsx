9c47e05d5dcb6d4e6ad10d928c67d07549b56ff4
sohostory <sohostory@gmail.com>
2024-04-14 10:25:41 -0500

    receipe card container created

---
+++ Frontend_Application/src/components/recipe-card-container.component.jsx
@@ -0,0 +1,13 @@
+import RecipeCard from "./recipe-card.component.jsx";
+
+const dbMock = [];
+
+function RecipeCardContainer(props) {
+  return (
+    <div>
+      <RecipeCard />
+    </div>
+  );
+}
+
+export default RecipeCardContainer;
