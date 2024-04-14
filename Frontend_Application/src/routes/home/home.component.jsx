f7356ce08143b7edd1fb0fd8e6417be8109aeb1b
sohostory <sohostory@gmail.com>
2024-04-14 15:59:40 -0500

    refactoring home page to show either dashboard or landing page, update with recipe cards once other PR is approved

--- Frontend_Application/src/routes/home/home.component.jsx
+++ Frontend_Application/src/routes/home/home.component.jsx
@@ -2,15 +2,19 @@ import { Outlet } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import pic from "../../assets/tomatoes.png";
 import Navbar from "../../components/navbar.component.jsx";
 import Landing from "../../components/landing.component.jsx";
+import DashboardPage from "../dasboard/dashboard.component.jsx";
 
 const Home = () => {
+  const userName = "";
   return (
     <div>
       <Outlet />
       <Navbar />
-      <Landing />
+      // If user is present, show dashboard // If not, place landing page
+      {userName ? <DashboardPage /> : <Landing />}
     </div>
   );
 };
