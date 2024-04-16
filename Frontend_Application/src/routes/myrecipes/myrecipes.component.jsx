7eb711390e3391cfb33bf67d63b4637841ee4c76
Michelle Song <michssong@gmail.com>
2024-04-16 00:15:35 -0500

    not my ticket so removed code

--- Frontend_Application/src/routes/myrecipes/myrecipes.component.jsx
+++ Frontend_Application/src/routes/myrecipes/myrecipes.component.jsx
@@ -3,9 +3,6 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Navbar from "../../components/navbar.component.jsx";
 import Menu from "../../components/menu.component";
-import SearchRecipesBar from "../../components/search-recipes-bar.component.jsx";
-import RecipeCard from "../../components/recipe-card.component.jsx";
-import RecipeCardContainer from "../../components/recipe-card-container.component.jsx";
 
 const MyRecipes = () => {
   return (
@@ -14,13 +11,9 @@ const MyRecipes = () => {
       <Navbar />
       <Grid container justifyContent='center' alignItems='center'>
         <Grid item>
-          <Typography variant='h3' gutterBottom sx={{ p: 4 }}>
+          <Typography variant='h3' gutterBottom>
             My Recipes
           </Typography>
-          <SearchRecipesBar />
-          <RecipeCardContainer>
-            <RecipeCard />
-          </RecipeCardContainer>
         </Grid>
         <Grid item>
           <Menu />
