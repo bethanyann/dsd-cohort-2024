ed3603a9a962ede54d7427caa8f3b13679716fee
diss <65290988+Dissurender@users.noreply.github.com>
2024-04-17 17:25:19 -0500

    Api responses added for more accurate docs

--- Backend_Application/src/main/java/dsd/cohort/application/user/UserController.java
+++ Backend_Application/src/main/java/dsd/cohort/application/user/UserController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 import dsd.cohort.application.ingredient.IngredientEntity;
 import dsd.cohort.application.recipe.RecipeEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -26,6 +30,11 @@ public class UserController {
         return userService.getAll();
     }
 
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "All users returned"),
+        @ApiResponse(responseCode = "500", description = "Could not create user")
+    })
     @PostMapping("/createuser")
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
 
@@ -39,6 +48,11 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Get a user by email")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User found"),
+        @ApiResponse(responseCode = "500", description = "User not found")
+    })
     @GetMapping("/finduserbyemail")
     public ResponseEntity<UserEntity> findUserByEmail(@RequestParam String email) {
         try {
@@ -52,29 +66,54 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Add a recipe to a users favorites")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Recipe added to favorites"),
+        @ApiResponse(responseCode = "500", description = "Recipe not added to favorites")
+    })
     // add a recipe to a users favorites
     @GetMapping("/addrecipe/{email}/{recipeId}")
     public boolean addRecipe(@PathVariable String email, @PathVariable String recipeId) {
         return userService.addRecipe(email, recipeId);
     }
 
+    @Operation(summary = "Delete a recipe from a users favorites")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Recipe deleted from favorites"),
+        @ApiResponse(responseCode = "500", description = "Recipe not deleted from favorites")
+    })
     // delete a recipe from a users favorites
     @DeleteMapping("/deleterecipe/{email}/{recipeId}")
     public boolean deleteRecipe(@PathVariable String email, @PathVariable String recipeId) {
         return userService.deleteRecipe(email, recipeId);
     }
 
+    @Operation(summary = "Get a users favorites")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Users favorites returned"),
+        @ApiResponse(responseCode = "500", description = "Could not get users favorites")
+    })
     // get a users favorites
     @GetMapping("/getuserfavorites/{email}")
     public Set<RecipeEntity> getUserFavorites(@PathVariable String email) {
         return userService.getUserFavorites(email);
     }
 
+    @Operation(summary = "Get a users grocery list")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Users grocery list returned"),
+        @ApiResponse(responseCode = "500", description = "Could not get users grocery list")
+    })
     @GetMapping("/getgrocerylist/{email}")
     public ResponseEntity<Set<IngredientEntity>> getGroceryList(@PathVariable String email) {
         return userService.getGroceryList(email);
     }
 
+    @Operation(summary = "Remove an ingredient to a users grocery list")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ingredient removed from grocery list"),
+        @ApiResponse(responseCode = "500", description = "Ingredient not removed from grocery list")
+    })
     @DeleteMapping("/removefromgrocerylist/{email}/{foodId}")
     public boolean removeFromGroceryList(@PathVariable String email, @PathVariable String foodId) {
         return userService.removeFromGroceryList(email, foodId);
