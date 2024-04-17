4f9af10d86371d752efeb02817437d54679110cf
diss <65290988+Dissurender@users.noreply.github.com>
2024-04-17 12:08:00 -0500

    remove comment on UserRepo and add Queries to RecipeRepo

--- Backend_Application/src/main/java/dsd/cohort/application/user/UserRepository.java
+++ Backend_Application/src/main/java/dsd/cohort/application/user/UserRepository.java
@@ -7,10 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-/**
- * This is a simple implement of the users repository that uses JPA
- * and has no frills or logic.
- */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query(value = "SELECT u FROM UserEntity u WHERE u.email=:email")
